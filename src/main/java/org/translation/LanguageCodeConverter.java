d083322a2b498c7ae68ba5bf99da2bbf4392054a
Hadi Ahmed <hadiahmed554@gmail.com>
2024-09-27 18:00:12 -0400

    idk

--- src/main/java/org/translation/LanguageCodeConverter.java
+++ src/main/java/org/translation/LanguageCodeConverter.java
@@ -8,72 +8,46 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * This class provides the service of converting language codes to their names.
- */
 public class LanguageCodeConverter {
 
-    private static Map<String, String> code_to_name = new HashMap<>();
+    private final Map<String, String> codeToLanguageMap = new HashMap<>();
+    private final Map<String, String> languageToCodeMap = new HashMap<>();
 
-    /**
-     * Default constructor which will load the language codes from "language-codes.txt"
-     * in the resources folder.
-     */
     public LanguageCodeConverter() {
         this("language-codes.txt");
     }
 
-    /**
-     * Overloaded constructor which allows us to specify the filename to load the language code data from.
-     * @param filename the name of the file in the resources folder to load the data from
-     * @throws RuntimeException if the resource file can't be loaded properly
-     */
     public LanguageCodeConverter(String filename) {
-
         try {
             List<String> lines = Files.readAllLines(Paths.get(getClass()
                     .getClassLoader().getResource(filename).toURI()));
             for (int i = 1; i < lines.size(); i++) {
-                String[] parts = lines.get(i).split("\t");
-                if (parts.length == 2) {
-                    code_to_name.put(parts[1], parts[0]);
+                String line = lines.get(i).trim();
+                if (line.isEmpty()) {
+                    continue;
                 }
-                else {
-                    // For some reason the english code is not getting picked up by \t
-                    String[] eng = parts[0].split(" ");
-
-                    code_to_name.put(eng[1], eng[0]);
+                String[] split = line.split("\t");
+                if (split.length >= 2) {
+                    String languageName = split[0];
+                    String languageCode = split[1];
+                    codeToLanguageMap.put(languageCode, languageName);
+                    languageToCodeMap.put(languageName, languageCode);
                 }
             }
-        }
-        catch (IOException | URISyntaxException ex) {
+        } catch (IOException | URISyntaxException ex) {
             throw new RuntimeException(ex);
         }
     }
 
-    /**
-     * Returns the name of the language for the given language code.
-     * @param code the language code
-     * @return the name of the language corresponding to the code
-     */
     public String fromLanguageCode(String code) {
-        return code_to_name.get(code);
+        return codeToLanguageMap.get(code);
     }
 
-    /**
-     * Returns the code of the language for the given language name.
-     * @param language the name of the language
-     * @return the 2-letter code of the language
-     */
     public String fromLanguage(String language) {
-        return code_to_name.get(language);
+        return languageToCodeMap.get(language);
     }
 
-    /**
-     * Returns how many languages are included in this code converter.
-     * @return how many languages are included in this code converter.
-     */
     public int getNumLanguages() {
-        return code_to_name.size();
+        return codeToLanguageMap.size();
     }
 }
--- src/main/java/org/translation/Main.java
+++ src/main/java/org/translation/Main.java
@@ -4,50 +4,42 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-/**
- * Main class for this program.
- * Complete the code according to the "to do" notes.<br/>
- * The system will:<br/>
- * - prompt the user to pick a country name from a list<br/>
- * - prompt the user to pick the language they want it translated to from a list<br/>
- * - output the translation<br/>
- * - at any time, the user can type quit to quit the program<br/>
- */
 public class Main {
 
     private static final String QUIT = "quit";
 
-    /**
-     * This is the main entry point of our Translation System!<br/>
-     * A class implementing the Translator interface is created and passed into a call to runProgram.
-     * @param args not used by the program
-     */
     public static void main(String[] args) {
-        Translator translator = new JSONTranslator(null);
+        Translator translator = new JSONTranslator();
         runProgram(translator);
     }
 
-    /**
-     * This is the method which we will use to test your overall program, since
-     * it allows us to pass in whatever translator object that we want!
-     * See the class Javadoc for a summary of what the program will do.
-     * @param translator the Translator implementation to use in the program
-     */
     public static void runProgram(Translator translator) {
+        CountryCodeConverter countryCodeConverter = new CountryCodeConverter();
+        LanguageCodeConverter languageCodeConverter = new LanguageCodeConverter();
+
         while (true) {
-            String country = promptForCountry(translator);
-            if (QUIT.equals(country)) {
+            String countryName = promptForCountry(translator);
+            if (QUIT.equals(countryName)) {
                 break;
             }
-            String language = promptForLanguage(translator, country);
-            if (QUIT.equals(language)) {
-                break;
+            String countryCode = countryCodeConverter.fromCountry(countryName);
+            if (countryCode == null) {
+                System.out.println("Invalid country name.");
+                continue;
             }
 
-            LanguageCodeConverter converter = new LanguageCodeConverter();
+            String languageName = promptForLanguage(translator, countryCode);
+            if (QUIT.equals(languageName)) {
+                break;
+            }
+            String languageCode = languageCodeConverter.fromLanguage(languageName);
+            if (languageCode == null) {
+                System.out.println("Invalid language name.");
+                continue;
+            }
 
-            System.out.println(country + " in " + language + " is " + translator.translate(country,
-                            converter.fromLanguage(language)));
+            String translation = translator.translate(countryCode, languageCode);
+            System.out.println(countryName + " in " + languageName + " is " + translation);
             System.out.println("Press enter to continue or quit to exit.");
             Scanner s = new Scanner(System.in);
             String textTyped = s.nextLine();
@@ -58,33 +50,36 @@ public class Main {
         }
     }
 
-    // Note: CheckStyle is configured so that we don't need javadoc for private methods
     private static String promptForCountry(Translator translator) {
-        List<String> countries = translator.getCountries();
-        List<String> countryCodes = new ArrayList<>(countries.size());
+        List<String> countryCodes = translator.getCountries();
         CountryCodeConverter countryCodeConverter = new CountryCodeConverter();
-        for (String country : countries) {
-            countryCodes.add(countryCodeConverter.fromCountryCode(country));
+        List<String> countryNames = new ArrayList<>();
+        for (String code : countryCodes) {
+            String name = countryCodeConverter.fromCountryCode(code);
+            if (name != null) {
+                countryNames.add(name);
+            }
         }
-        countryCodes.sort(String::compareTo);
-        countryCodes.forEach(System.out::println);
+        countryNames.sort(String::compareTo);
+        countryNames.forEach(System.out::println);
         System.out.println("select a country from above:");
 
         Scanner s = new Scanner(System.in);
         return s.nextLine();
-
     }
 
-    // Note: CheckStyle is configured so that we don't need javadoc for private methods
-    private static String promptForLanguage(Translator translator, String country) {
+    private static String promptForLanguage(Translator translator, String countryCode) {
+        List<String> languageCodes = translator.getCountryLanguages(countryCode);
         LanguageCodeConverter languageCodeConverter = new LanguageCodeConverter();
-        List<String> langs = new ArrayList<>();
-        for (String lang : translator.getCountryLanguages(country)) {
-            langs.add(languageCodeConverter.fromLanguageCode(lang));
+        List<String> languageNames = new ArrayList<>();
+        for (String code : languageCodes) {
+            String name = languageCodeConverter.fromLanguageCode(code);
+            if (name != null) {
+                languageNames.add(name);
+            }
         }
-
-        langs.sort(String::compareTo);
-        langs.forEach(System.out::println);
+        languageNames.sort(String::compareTo);
+        languageNames.forEach(System.out::println);
         System.out.println("select a language from above:");
 
         Scanner s = new Scanner(System.in);
